a17dc9402972443fb7ce8e3e51ff52372b3acafa
taji <taji@sonicgarden.jp>
2024-06-14 16:28:18 +0900

    複合indexを追加する

--- db/migrate/20240614072648_create_likes.rb
+++ db/migrate/20240614072648_create_likes.rb
@@ -5,6 +5,7 @@ class CreateLikes < ActiveRecord::Migration[7.1]
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
+      t.index %i[user_id post_id], unique: true
     end
   end
 end
