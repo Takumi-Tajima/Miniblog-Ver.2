4ac194241c43c97c5ba1c052181bc8e3d528157e
taji <taji@sonicgarden.jp>
2024-06-18 16:06:55 +0900

    likes_controller find_byで例外処理

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -5,7 +5,7 @@ class Posts::LikesController < ApplicationController
   end
 
   def destroy
-    @like_post = current_user.likes.find_by(post_id: params[:post_id])
+    @like_post = current_user.likes.find_by!(post_id: params[:post_id])
     @like_post.destroy!
     redirect_to request.referer || root_path
   end
