767cbc7f663dd4241a686b78713ac58d9de2a4f9
taji <taji@sonicgarden.jp>
2024-06-18 11:20:36 +0900

    likes_controller ディレクトリ構成を修正

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -1,7 +1,6 @@
-class Users::LikesController < ApplicationController
+class Posts::LikesController < ApplicationController
   def create
-    @like_post = Like.new(user_id: current_user.id, post_id: params[:post_id])
-    @like_post.save
+    @like_post = current_user.likes.create!(post_id: params[:post_id])
     redirect_to request.referer || root_path
   end
 
