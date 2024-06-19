93ca73d98f56f23b1cec0343aa919a5bb05bd8a6
taji <taji@sonicgarden.jp>
2024-06-19 20:02:24 +0900

    フォロー中のユーザーのみの投稿一覧でもいいねを読み込ませる

--- app/controllers/users/following_posts_controller.rb
+++ app/controllers/users/following_posts_controller.rb
@@ -1,5 +1,6 @@
 class Users::FollowingPostsController < ApplicationController
   def index
     @posts = Post.where(user_id: current_user.following_ids).default_order
+    @liked_post_ids = current_user&.likes&.pluck(:post_id)
   end
 end
