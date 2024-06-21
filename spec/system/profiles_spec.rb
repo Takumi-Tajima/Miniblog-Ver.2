1b5e4c49e6bdf13d013b6ebece269679cb80e8c9
taji <taji@sonicgarden.jp>
2024-06-21 11:23:41 +0900

    他人のプロフィールのテストを追加

--- spec/system/profiles_spec.rb
+++ spec/system/profiles_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'プロフィール機能', type: :system do
     sign_in user
   end
 
-  context 'ログインしている時' do
+  context '自分のプロフィールの時' do
     it '自分のプロフィールを確認できること' do
       visit root_path
       within('.nav') do
@@ -16,6 +16,37 @@ RSpec.describe 'プロフィール機能', type: :system do
       expect(page).to have_content 'jojo'
       expect(page).to have_content '私の名前は空条承太郎です'
       expect(page).to have_content 'google.com'
+    end
+
+    it '自分のプロフィールを編集できること' do
+      visit profile_path
+      click_on '編集'
+      fill_in '自己紹介',	with: '自己紹介をします'
+      fill_in 'user_blog_url', with: 'taji.blog'
+      click_on '更新する'
+      expect(page).to have_content '自己紹介をします'
+      expect(page).to have_content 'taji.blog'
+      expect(page).not_to have_content '私の名前は空条承太郎です'
+      expect(page).not_to have_content 'google.com'
+    end
+  end
+
+  context '他人のプロフィールの時' do
+    let(:other) { create(:user, name: 'dio', about_me: '私の名前はdioです', blog_url: 'taji.blog') }
+
+    before do
+      create(:post, user: other, content: 'wryyyyyyy')
+    end
+
+    it '他人のプロフィールを確認できること' do
+      visit root_path
+      within '.list-group-item' do
+        expect(page).to have_content 'wryyyyyyy'
+        click_on 'dio'
+      end
+      expect(page).to have_content 'dio'
+      expect(page).to have_content '私の名前はdioです'
+      expect(page).to have_content 'taji.blog'
     end
   end
 end
