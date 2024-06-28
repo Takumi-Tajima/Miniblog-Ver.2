db934fb3c2d3c9f464d50e3d12c149e8a91ba689
taji <taji@sonicgarden.jp>
2024-06-28 14:07:05 +0900

    comments_controllerのコメントアウトを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,16 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show edit update destroy]
 
-  # GET /comments/1
   def show
   end
 
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit
   end
 
-  # POST /comments
   def create
     @comment = Comment.new(comment_params)
 
@@ -25,7 +21,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
   def update
     if @comment.update(comment_params)
       redirect_to @comment, notice: 'Comment was successfully updated.', status: :see_other
@@ -34,7 +29,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
   def destroy
     @comment.destroy!
     redirect_to comments_url, notice: 'Comment was successfully destroyed.', status: :see_other
@@ -42,12 +36,10 @@ class CommentsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = Comment.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def comment_params
     params.require(:comment).permit(:user_id, :post_id, :content)
   end
