612e6aa573e799e902576bc4050c89b249a49f0b
taji <taji@sonicgarden.jp>
2024-07-02 10:55:38 +0900

    comments_controllerを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
+  before_action :set_post, only: %i[new create update destroy]
 
   def new
-    @comment = Comment.new
+    @comment = @post.comments.build
   end
 
   def create
-    @comment = Comment.new(comment_params)
+    @comment = @post.comments.build(comment_params.merge(user: current_user))
 
     if @comment.save
-      redirect_to @comment, notice: t('controllers.common.created', model: 'コメント')
+      redirect_to post_path(@post), notice: t('controllers.common.created', model: 'コメント')
     else
       render :new, status: :unprocessable_entity
     end
@@ -20,7 +21,7 @@ class CommentsController < ApplicationController
 
   def update
     if @comment.update(comment_params)
-      redirect_to @comment, notice: t('controllers.common.update', model: 'コメント'), status: :see_other
+      redirect_to post_path(@post), notice: t('controllers.common.update', model: 'コメント'), status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -28,13 +29,17 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment.destroy!
-    redirect_to comments_url, notice: t('controllers.common.destroyed', model: 'コメント'), status: :see_other
+    redirect_to post_path(@post), notice: t('controllers.common.destroyed', model: 'コメント'), status: :see_other
   end
 
   private
 
   def set_comment
     @comment = Comment.find(params[:id])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
   def comment_params
